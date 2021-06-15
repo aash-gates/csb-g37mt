68d62490da656d6c7aa2a460d27ab3346f44bd51
Code Sandbox <hello@codesandbox.io>
2021-06-15 18:25:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.c319514b00ca056fcf7e60325545bfd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1534e27cb83cb484ef07dcb25c3feb",
+    "url": "https://aash-gates.github.io/csb-g37mt/index.html"
+  },
+  {
+    "revision": "ce97be33124772506a52",
+    "url": "https://aash-gates.github.io/csb-g37mt/static/css/main.ce9d4054.chunk.css"
+  },
+  {
+    "revision": "fdb31f8a1688d5f98327",
+    "url": "https://aash-gates.github.io/csb-g37mt/static/js/2.6eaac070.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://aash-gates.github.io/csb-g37mt/static/js/2.6eaac070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce97be33124772506a52",
+    "url": "https://aash-gates.github.io/csb-g37mt/static/js/main.59e0f045.chunk.js"
+  },
+  {
+    "revision": "6d40bd1ae36d2146a1d6",
+    "url": "https://aash-gates.github.io/csb-g37mt/static/js/runtime-main.f0deb346.js"
+  }
+]);
